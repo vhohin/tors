d056139aa8322d41912863b0c29907ccbe626835
210-06\ipd <vhohin@gmail.com>
2016-10-07 14:24:20 -0400

    no message

--- index.php
+++ index.php
@@ -232,8 +232,16 @@ $app->post('/select', function() use ($app, $log) {
 });
 //**************************************************** Selected bus
 
-$app->get('/selectbus', function() use ($app, $log) {
-    $app->render('selected_bus.html.twig', array('currentUser' => $_SESSION['user']));
+$app->get('/selectbus/:BusID', function($BusID) use ($app, $log) {
+    $bus = DB::queryFirstRow('SELECT * FROM buses WHERE ID=%d', $BusID);
+    if (!$bus) {
+        $log->debug("ERROR: Not found bus for selected destination");
+    }
+    $seats = DB::query('SELECT * FROM seats WHERE BusID=%d', $BusID);
+    if (!$seats) {
+        $log->debug("ERROR: Not found seats for selected bus");
+    }
+    $app->render('selected_bus.html.twig', array('bus'=>$bus, 'currentUser' => $_SESSION['user']));
 });
 
 //**************************************************** REGISTER
@@ -356,61 +364,14 @@ $app->get('/profile', function() use ($app, $log) {
 });
 
 $app->get('/passwordReset', function() use ($app, $log) {
+    //
+    $result = mail("vhohin@gmail.com", "Password confirmation", "Your login: kuku \nYour password: no known ","From: tors@gmail.com \r\n");
     $app->render('passwordReset.html.twig');
 });
 
 
 $app->post('/passwordReset', function() use ($app, $log) {
-    //include "connect.php"; //connects to the database
-    if (isset($_POST['username'])) {
-        //$username = $_POST['username'];
-        //$query = "SELECT * FROM users WHERE username='$username'";
-        
-         
-        //$result = mysqli_query($connection, $query) or die(mysqli_error($connection));
-       // $count = mysqli_num_rows($result);
-        // If the count is equal to one, we will send message other wise display an error message.
-        
-         $username = $app->request->post('username');
-         $user = DB::queryFirstRow("SELECT * FROM users WHERE username==%s", $username);
     
-        if ($user) {
-            //$rows = mysqli_fetch_array($result);
-            $pass = $user['password']; //FETCHING PASS
-            //echo "your pass is ::".($pass)."";
-            $to = $user['email'];
-            //echo "your email is ::".$email;
-            //Details for sending E-mail
-            $from = "TORS";
-            $url = "http://tors.ipd8.ingo/";
-            $body = "TORS password recovery Script
-		-----------------------------------------------
-		Url : $url;
-		email Details is : $to;
-		Here is your password  : $pass;
-		Sincerely,
-		TORS";
-            $from = "Your-email-address@domaindotcom";
-            $subject = "TORS Password recovered";
-            $headers1 = "From: $from\n";
-            $headers1 .= "Content-type: text/html;charset=iso-8859-1\r\n";
-            $headers1 .= "X-Priority: 1\r\n";
-            $headers1 .= "X-MSMail-Priority: High\r\n";
-            $headers1 .= "X-Mailer: Just My Server\r\n";
-            $sentmail = mail($to, $subject, $body, $headers1);
-        } else {
-            if ($_POST ['email'] != "") {
-                $fmsg = "Not found your email in our database";
-            }
-        }
-        //If the message is sent successfully, display sucess message otherwise display an error message.
-        if ($sentmail == 1) {
-            $smsg = "Your Password Has Been Sent To Your Email Address.";
-        } else {
-            if ($_POST['email'] != "")
-                $nmsg = "Cannot send password to your e-mail address.Problem with sending mail...";
-        }
-    }
 });
 
 
