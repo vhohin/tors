c398b72884256386e108bc143b3c58ad341747d7
Valeriy Hohin <vhohin@gmail.com>
2016-10-15 07:54:12 -0400

    no message

--- cache/9b/9b22f0c2b92fbd4dd90666e5a314c5c9fee01ede8b82fc68c256f5cbdeb758e2.php
+++ cache/9b/9b22f0c2b92fbd4dd90666e5a314c5c9fee01ede8b82fc68c256f5cbdeb758e2.php
@@ -32,43 +32,83 @@ class __TwigTemplate_940d965ecc5909e9df61df45bbdfd9e74f27f73ba332ff616e5890f816d
         <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
         <link rel=\"stylesheet\" href=\"/styles.css\" />
         <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js\"></script>
-";
-        // line 10
-        $this->displayBlock('head', $context, $blocks);
+
+    ";
         // line 11
-        echo "    </head>
-    <body>
-        <div id=\"header\">
-            <div id=\"logo\">
-                <img src=\"/images/ticket5.png\"  alt=\"Logo\">
-                <h1>Reservation Service</h1>
-            </div>            
-            <div id=\"topMenu\">
-                <ul>
-                    <li><a href=\"\" >HELP</a></li>
-                    <li><a href=\"\" >CONTACT AS</a></li>
-                    <li><a href=\"\" >ABOUT AS</a></li>
-                </ul>
-            </div>
-        </div>   
-        <div id=\"centerContent\">
-             
-<div id=\"content\">
-    <div id=\"loginBlock\">";
-        // line 29
+        $this->displayBlock('head', $context, $blocks);
+        // line 12
+        echo "</head>
+<body>
+    <div id=\"header\">
+        <div id=\"logo\">
+            <img src=\"/images/ticket5.png\"  alt=\"Logo\">
+            <h1>Reservation Service</h1>
+        </div>            
+        <div id=\"topMenu\">
+            <ul>
+                <li><a href=\"\" >HELP</a></li>
+                <li><a href=\"\" >CONTACT AS</a></li>
+                <li><a href=\"\" >ABOUT AS</a></li>
+            </ul>
+        </div>
+    </div>   
+    <div id=\"centerContent\">
+
+        <div id=\"content\">
+            <div id=\"loginBlock\">";
+        // line 30
         $this->displayBlock('login', $context, $blocks);
         echo "</div>    
-";
-        // line 30
-        $this->displayBlock('content', $context, $blocks);
+        ";
         // line 31
-        echo " </div>
-            
-        </div>
+        $this->displayBlock('content', $context, $blocks);
+        // line 32
+        echo "    </div>
+
+</div>
+<div id=\"footer\">                
+    &copy; Copyright 2016 by <a href=\"/\">TORS</a>.                
+</div>
+
+
+<script>
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function () {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-85707574-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
+
+
+</body>
+<!-- valeriy
         <div id=\"footer\">                
             &copy; Copyright 2016 by <a href=\"/\">TORS</a>.                
         </div>
+ 
+ <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-85701035-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
     </body>
+-->
 </html>
 ";
     }
@@ -78,17 +118,17 @@ class __TwigTemplate_940d965ecc5909e9df61df45bbdfd9e74f27f73ba332ff616e5890f816d
     {
     }
 
-    // line 10
+    // line 11
     public function block_head($context, array $blocks = array())
     {
     }
 
-    // line 29
+    // line 30
     public function block_login($context, array $blocks = array())
     {
     }
 
-    // line 30
+    // line 31
     public function block_content($context, array $blocks = array())
     {
     }
@@ -100,7 +140,7 @@ class __TwigTemplate_940d965ecc5909e9df61df45bbdfd9e74f27f73ba332ff616e5890f816d
 
     public function getDebugInfo()
     {
-        return array (  92 => 30,  87 => 29,  82 => 10,  77 => 5,  65 => 31,  63 => 30,  59 => 29,  39 => 11,  37 => 10,  29 => 5,  23 => 1,);
+        return array (  132 => 31,  127 => 30,  122 => 11,  117 => 5,  66 => 32,  64 => 31,  60 => 30,  40 => 12,  38 => 11,  29 => 5,  23 => 1,);
     }
 
     public function getSource()
@@ -114,34 +154,74 @@ class __TwigTemplate_940d965ecc5909e9df61df45bbdfd9e74f27f73ba332ff616e5890f816d
         <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
         <link rel=\"stylesheet\" href=\"/styles.css\" />
         <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js\"></script>
-{% block head %}{% endblock %}
-    </head>
-    <body>
-        <div id=\"header\">
-            <div id=\"logo\">
-                <img src=\"/images/ticket5.png\"  alt=\"Logo\">
-                <h1>Reservation Service</h1>
-            </div>            
-            <div id=\"topMenu\">
-                <ul>
-                    <li><a href=\"\" >HELP</a></li>
-                    <li><a href=\"\" >CONTACT AS</a></li>
-                    <li><a href=\"\" >ABOUT AS</a></li>
-                </ul>
-            </div>
-        </div>   
-        <div id=\"centerContent\">
-             
-<div id=\"content\">
-    <div id=\"loginBlock\">{% block login %}{% endblock %}</div>    
-{% block content %}{% endblock %}
- </div>
-            
+
+    {% block head %}{% endblock %}
+</head>
+<body>
+    <div id=\"header\">
+        <div id=\"logo\">
+            <img src=\"/images/ticket5.png\"  alt=\"Logo\">
+            <h1>Reservation Service</h1>
+        </div>            
+        <div id=\"topMenu\">
+            <ul>
+                <li><a href=\"\" >HELP</a></li>
+                <li><a href=\"\" >CONTACT AS</a></li>
+                <li><a href=\"\" >ABOUT AS</a></li>
+            </ul>
         </div>
+    </div>   
+    <div id=\"centerContent\">
+
+        <div id=\"content\">
+            <div id=\"loginBlock\">{% block login %}{% endblock %}</div>    
+        {% block content %}{% endblock %}
+    </div>
+
+</div>
+<div id=\"footer\">                
+    &copy; Copyright 2016 by <a href=\"/\">TORS</a>.                
+</div>
+
+
+<script>
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function () {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-85707574-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
+
+
+</body>
+<!-- valeriy
         <div id=\"footer\">                
             &copy; Copyright 2016 by <a href=\"/\">TORS</a>.                
         </div>
+ 
+ <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-85701035-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
     </body>
+-->
 </html>
 ";
     }
